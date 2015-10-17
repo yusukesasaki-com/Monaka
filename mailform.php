3c13ea37b82c939b1a54a6a1a0b1e467d31154e5
yusukesasaki-com <yusukesasaki.com@gmail.com>
2015-10-17 17:02:54 +0900

    mailfrom.php css リンク

--- mailform.php
+++ mailform.php
@@ -3,6 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>メールフォーム</title>
+    <link rel="stylesheet" href="css/html5reset-1.6.1.css">
+    <link rel="stylesheet" href="css/ini.css">
+    <link rel="stylesheet" href="css/mailform.css">
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+    <![endif]-->
 </head>
 <body>
     
