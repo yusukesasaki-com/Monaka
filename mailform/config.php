bcd9caf584ccea47da8f79bb3f138311232bd1d4
yusukesasaki-com <yusukesasaki.com@gmail.com>
2015-10-21 01:01:27 +0900

    送信機能実装(未テスト)

---
+++ mailform/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// 送信先メールアドレス
+define(ADMIN_MAIL, "");
+
+// 送信後に戻るURL
+define(RE_URL, "");
+
+// 送信完了メッセージ
+$completionMessage = <<<EOD
+送信が完了しました。
+ありがとうございます。
+EOD
+;
--- mailform/send.php
+++ mailform/send.php
@@ -12,5 +12,59 @@ checkToken();
 // ----------CSRF対策終了---------- //
 
 
+$submitContent = array();
+$submitContent = $_POST;
+
+
+// メールの言語・文字コードの設定
+mb_language("Japanese");
+mb_internal_encoding("UTF-8");
+
+// タイトルの設定
+$send_title = "{$submitContent["name"]}様よりお問い合わせ";
+mb_encode_mimeheader($send_title, "ISO-2022-JP");
+
+// メッセージの設定
+$send_message = "{$submitContent["name"]}様より、下記内容でお問い合わせが届いています。\n";
+$send_message .= "\n";
+$send_message .= "■メールアドレス\n";
+$send_message .= "{$submitContent["mail"]}\n\n";
+$send_message .= "■お問い合わせ内容\n";
+$send_message .= "{$submitContent["comment"]}";
+$send_message = mb_convert_encoding($send_message, "ISO-2022-JP","UTF-8");
+
+// メールの送信 (宛先, 件名, 本文, 送り主(From:が必須))
+@mb_send_mail(ADMIN_MAIL, $send_title, $send_message, "From:{$submitContent["mail"]}");
 
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>メールフォーム</title>
+    <link rel="stylesheet" href="../css/html5reset-1.6.1.css">
+    <link rel="stylesheet" href="../css/ini.css">
+    <link rel="stylesheet" href="../css/mailform.css">
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+    <![endif]-->
+</head>
+<body>
+    
+<div class="container">
+    
+    <h1><span>送信完了</span></h1>
+    
+    <p class="completion">
+        <?php echo nl2br(h($completionMessage)); ?>
+    </p>
+    
+    <div class="submit_area">
+        <input class="single" type="button" value="戻る" onclick="window.location='<?php echo RE_URL; ?>';">
+    </div>
+    
+</div>
+    
+</body>
+</html>
