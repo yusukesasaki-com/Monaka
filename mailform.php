a534934f3dd8a1a85e4b892979cecb7a47f5abe2
yusukesasaki <yusukesasaki.com@gmail.com>
2015-10-26 01:10:39 +0900

    checkboxに対応して動作チェック済み + 見た目少し変更

--- mailform.php
+++ mailform.php
@@ -20,19 +20,19 @@
     <div class="mailform">
         <form action="./mailform/confirmation.php" method="post">
             <dl>
-                <dt>お名前</dt>
+                <dt>■お名前</dt>
                 <dd>
                     <input type="text" name="お名前[value]">
                     <input type="hidden" name="お名前[params]" value="名前">
                 </dd>
                 
-                <dt>メールアドレス</dt>
+                <dt>■メールアドレス</dt>
                 <dd>
                     <input type="text" name="メールアドレス[value]">
                     <input type="hidden" name="メールアドレス[params]" value="メール">
                 </dd>
                 
-                <dt>年齢</dt>
+                <dt>■年齢</dt>
                 <dd>
                     <label><input type="radio" name="年齢[value]" value="～20" checked> ～20</label>　
                     <label><input type="radio" name="年齢[value]" value="20代"> 20代</label>　
@@ -40,13 +40,38 @@
                     <label><input type="radio" name="年齢[value]" value="40代"> 40代</label>　
                     <label><input type="radio" name="年齢[value]" value="50～"> 50～</label>
                     </select>
+                    <input type="hidden" name="年齢[params]" value="">
                 </dd>
                 
-                <dt>お問い合わせ内容</dt>
+                <dt>■当サイトを知ったきっかけ</dt>
+                <dd>
+                    <label><input type="checkbox" name="当サイトを知ったきっかけ[value][]" value="検索"> 検索</label><br>
+                    <label><input type="checkbox" name="当サイトを知ったきっかけ[value][]" value="ブログ"> ブログ</label><br>
+                    <label><input type="checkbox" name="当サイトを知ったきっかけ[value][]" value="その他"> その他</label>　
+                    <input type="hidden" name="当サイトを知ったきっかけ[params]" value="">
+                    
+                    <input type="text" name="当サイトを知ったきっかけ-その他[value]">
+                    <input type="hidden" name="当サイトを知ったきっかけ-その他[params]" value="">
+                    
+                    <div class="nest">
+                        <p>&lt;検索サイト&gt;</p>
+                        <label><input type="checkbox" name="検索サイト[value][]" value="Yahoo"> Yahoo</label>
+                        <label><input type="checkbox" name="検索サイト[value][]" value="Google"> Google</label><br>
+                        <label><input type="checkbox" name="検索サイト[value][]" value="その他"> その他</label>　
+                        <input type="hidden" name="検索サイト[params]" value="">
+                        
+                        <input type="text" name="検索サイト-その他[value]">
+                        <input type="hidden" name="検索サイト-その他[params]" value="">
+                    </div>
+                    
+                </dd>
+        
+                <dt>■お問い合わせ内容</dt>
                 <dd>
                     <textarea name="お問い合わせ内容[value]"></textarea>
                     <input type="hidden" name="お問い合わせ内容[params]" value="必須">
                 </dd>
+                
             </dl>
             
             <div class="submit_area">
--- mailform/confirmation.php
+++ mailform/confirmation.php
@@ -11,10 +11,8 @@ setToken();
 
 // ----------CSRF対策終了---------- //
 
-
 $requiredItem = array();
 $submitContent = array();
-
 
 // ----------パラメータチェック・エラーチェック開始---------- //
 
@@ -25,6 +23,11 @@ $mailCheck = false;
 foreach($_POST as $key => $values){
     
     // $params = explode(",", $values["params"]); /* 今後in_arrayと組み合わせて使うかも?*/
+    
+    // 配列(checkbox)を変数に変換
+    if(is_array($values["value"])){
+        $values["value"] = implode("、", $values["value"]);
+    }
     
     // 名前チェック
     if(strpos($values["params"], "名前") !== false){
@@ -112,7 +115,7 @@ if(!$nameCheck || !$mailCheck){
         <form action="send.php" method="post">
             <dl>
                 <?php foreach($submitContent as $key => $value): ?>
-                <dt><?php echo h($key); ?>:</dt>
+                <dt>■<?php echo h($key); ?></dt>
                 <dd>
                     <p>
                         <?php
